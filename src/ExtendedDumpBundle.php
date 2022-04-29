0511208c137bd7b4ed3cfb02847b4348373012bf
szczyglis <szczyglis@protonmail.com>
2022-04-29 02:17:08 +0200

    updated README

--- src/ExtendedDumpBundle.php
+++ src/ExtendedDumpBundle.php
@@ -24,6 +24,6 @@ use \Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ExtendedDumpBundle extends Bundle
 {
-    public const VERSION = '1.0.24';
+    public const VERSION = '1.0.25';
     public const VERSION_BUILD = '2022-04-29';
 }
