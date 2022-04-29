8cfffb881464f482d3ee2ecee6a3eab903ed98ce
Marcin Szczygliński <61396542+szczyglis-dev@users.noreply.github.com>
2022-04-29 03:21:21 +0200

    Update ExtendedDumpBundle.php

--- src/ExtendedDumpBundle.php
+++ src/ExtendedDumpBundle.php
@@ -24,6 +24,6 @@ use \Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ExtendedDumpBundle extends Bundle
 {
-    public const VERSION = '1.0.29';
+    public const VERSION = '1.0.30';
     public const VERSION_BUILD = '2022-04-29';
 }
